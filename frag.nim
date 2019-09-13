95091e80bb0a985f6b9225ba2706a2e24c0b28cf
carterza <carterza@gmail.com>
2019-09-13 19:24:23 +0300

    game loop

--- frag.nim
+++ frag.nim
@@ -1,6 +1,41 @@
-import framebuffer
+import framebuffer, window,
+       sdl2 as sdl
+
+const
+  physDeltaTime = 1.0 / 60.0
+  slowestFrame = 1.0 / 15.0
+
+var
+  deltaTime, accumulator: float64
 
 proc frag() =
+  if not window.init(960, 540, "frag"):
+    return
+
   framebuffer.init()
+
+  var 
+    lastFrameTime = sdl.getPerformanceCounter()
+    running = true
+    e: sdl.Event
+  
+  while running:
+    var currentFrameTime = sdl.getPerformanceCounter()
+    deltaTime = float64(currentFrameTime - lastFrameTime) / float64(sdl.getPerformanceCounter())
+    lastFrameTime = currentFrameTime
+
+    if deltaTime > slowestFrame:
+      deltaTime = slowestFrame
+
+    accumulator += deltaTime
+    while accumulator >= physDeltaTime:
+      while sdl.pollEvent(e):
+        case e.kind
+        of sdl.QuitEvent:
+          running = false
+        else:
+          discard
+    
+    accumulator -= physDeltaTime
 
 frag()
---
+++ window.nim
@@ -0,0 +1,78 @@
+import strformat,
+       bgfxdotnim, bgfxdotnim/platform, sdl2 as sdl
+
+type
+  Window = object
+    windowPtr: sdl.WindowPtr
+    width: int
+    height: int
+
+const
+  sdlMajorVersion* = 2
+  sdlMinorVersion* = 0
+  sdlPatchLevel* = 10
+  defaultFlags = SDL_WINDOW_SHOWN or SDL_WINDOW_RESIZABLE
+
+template sdlVersion*(x: untyped) =
+  (x).major = sdlMajorVersion
+  (x).minor = sdlMinorVersion
+  (x).patch = sdlPatchLevel
+
+when defined(windows):
+  type
+    SysWMMsgWinObj* = object
+      window*: pointer
+
+    SysWMInfoKindObj* = object
+      win*: SysWMMsgWinObj
+
+var display: Window
+
+proc linkSDL2BGFX(): bool =
+  var pd: bgfx_platform_data_t
+  var info: sdl.WMinfo
+  sdlVersion(info.version)
+  if not sdl.getWMInfo(display.windowPtr, info):
+    return result
+  
+  case(info.subsystem):
+    of SysWM_Windows:
+      when defined(windows):
+        let info = cast[ptr SysWMInfoKindObj](addr info.padding[0])
+        pd.nwh = cast[pointer](info.win.window)
+      pd.ndt = nil
+    else:
+      discard
+
+  pd.backBuffer = nil
+  pd.backBufferDS = nil
+  pd.context = nil
+  bgfx_set_platform_data(addr pd)
+  result = true
+
+proc init*(width, height: int; title: string): bool =
+  if sdl.init(INIT_VIDEO) != SdlSuccess:
+    echo &"Failed to initialize SDL:\n {sdl.getError()}"
+    return result
+  
+  display.windowPtr = sdl.createWindow(
+    title,
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    cint(width),
+    cint(height),
+    defaultFlags
+  )
+
+  if display.windowPtr.isNil:
+    echo &"Failed to create SDL window:\n {sdl.getError()}"
+    return result
+
+  if not linkSDL2BGFX():
+    echo &"Failed to retrieve driver specific info from SDL window:\n {sdl.getError()}"
+    return result
+
+  display.width = width
+  display.height = height
+
+  result = true
